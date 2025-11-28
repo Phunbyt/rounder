91052fd670a6110cb7742fe8deede3963fe09bda
Oluwafunbi Raji <roluwafunbi@gmail.com>
2025-11-28 17:44:31 +0100

    modified hero

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -14,15 +14,33 @@ const Hero = () => {
       }}
       disableGutters={true}
     >
+        
       <Box
         sx={{
           display: "flex",
+                      position: "relative",
+          
           flexDirection: { xs: "column", md: "row" }, // Stack vertically on small screens, row on larger screens
           justifyContent: "space-between",
           gap: { xs: "2em", md: "4em" }, // Responsive gap between text and image
           width: "100%",
         }}
       >
+        {/* Background Blur Effect */}
+                <Box
+                  sx={{
+                    position: "absolute",
+                    height: { xs: "150px", sm: "200px", md: "300px" }, // Responsive height
+                    width: "100%",
+                    maxWidth: { xs: "100px", sm: "150px", md: "200px" }, // Responsive width
+                    top: { xs: "10%", sm: "5%" }, // Responsive position
+                    left: { xs: "30%", sm: "40%" }, // Responsive position
+                    borderRadius: "50%",
+                    filter: "blur(80px)",
+                    background: "rgba(208, 53, 255, 0.21)",
+                    zIndex: -1, // Ensure it stays behind the content
+                  }}
+                ></Box>
         {/* Left Side: Text and Buttons */}
         <Box
           sx={{
@@ -31,7 +49,7 @@ const Hero = () => {
           }}
         >
           <HeaderText
-            text={"Redefining the Future of Agricultural Transportation"}
+            text={"Optimize Your Stock. Minimise Waste. Automate Alerts."}
             style={{
               fontSize: { xs: "1.5rem", sm: "2rem", md: "3rem" }, // Responsive font size
               margin: { xs: "0.5em auto", md: "1em auto" }, // Responsive margin
@@ -39,7 +57,7 @@ const Hero = () => {
           />
           <BodyText
             text={
-              "At RoundeR we provide a seamless eco-friendly and cost effective alternative for the agricultural supply chain"
+              "Automated inventory insights that detect low stock, track expiry dates, and highlight products ready for promotion."
             }
             style={{
               fontSize: { xs: "0.9rem", sm: "1rem", md: "1.2rem" }, // Responsive font size
@@ -76,7 +94,7 @@ const Hero = () => {
         </Box>
 
         {/* Right Side: Image */}
-        <Box
+        {/* <Box
           sx={{
             flex: 1,
             display: "flex",
@@ -95,7 +113,7 @@ const Hero = () => {
               borderRadius: "8px", // Optional: Add rounded corners
             }}
           />
-        </Box>
+        </Box> */}
       </Box>
     </Container>
   );
